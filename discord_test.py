391593f53a497f8767e5fa0094cadb63fb3e4128
vivianliu0 <56518575+vivianliu0@users.noreply.github.com>
2020-01-18 14:59:29 -0500

    Add files via upload

---
+++ discord_test.py
@@ -0,0 +1,17 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run('NjY3NzYzNDA4NjQ4Nzk4MjU4.XiNaAQ.kq9SiwLzRWndvtr__g5bsThR30k')
